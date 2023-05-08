23e8a2b349a33125c939cff13660739c079cde22
lookfortonylong <113322032+lookfortonylong@users.noreply.github.com>
2023-05-08 19:57:38 +0700

    Update install.sh

--- k8s/helm/wordpress/install.sh
+++ k8s/helm/wordpress/install.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 helm repo add stable https://kubernetes-charts.storage.googleapis.com
-helm install my-release bitnami/wordpress -f values.yaml
+helm install my-wordpress bitnami/wordpress -f values.yaml
